b552b5b12133f85272ee7c602d42ba329370d21d
Jesper Fyhr Knudsen <jesper@fyhr.dk>
2025-09-13 13:16:01 +0200

    add create book mode

---
+++ personalLibrary/src/main/java/dk/fyhr/kotlin/personalLibrary/Book.kt
@@ -0,0 +1,8 @@
+package dk.fyhr.kotlin.personalLibrary.dk.fyhr.kotlin.personalLibrary
+
+class Book (
+    val title: String,
+    val numberOfPages: Int,
+    val price: Int,
+    val condition: String
+)
